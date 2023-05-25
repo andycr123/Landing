17953b2e2966de3be9ad4f34e6a649f277cd6d9c
Andy c <81699558+andycr123@users.noreply.github.com>
2023-05-25 11:08:28 -0500

    changes

--- landing-andy/pages/index.tsx
+++ landing-andy/pages/index.tsx
@@ -3,15 +3,16 @@ import ContactMe from '@/components/ContactMe';
 import Footer from '@/components/Footer';
 import Hero from '@/components/Hero';
 import Projects from '@/components/Projects';
-import Technologies from '@/components/Technologies';
+import Technologies from '@/components/Techonlogies';
+
 // import Layout from '@/layout/LayoutSections';
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <AboutMe /> 
-      <Technologies />
+      <AboutMe/>
+      <Technologies/>
       <Projects />
       <ContactMe />
       <Footer />
